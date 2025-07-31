a0ee7b866cc4004db761abce6ee1a1e127db63bd
amirizo <amirmussa2003@gmail.com>
2025-07-31 11:47:25 +0300

    first commit

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    project: {
-      ios: {},
-      android: {},
-    },
-    assets: ['./assets/fonts'],
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts'],
+  experimental: {
+    enableFabric: false,
+    enableTurboModules: false,
+  },
 };
